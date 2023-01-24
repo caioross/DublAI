75598d05a7c108ef7b5ef100592d39d2e1c9ab9c
Caio Ross <49957185+caioross@users.noreply.github.com>
2023-01-24 06:00:09 -0300

    Update dublai.py

--- dublai.py
+++ dublai.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 import os
+import librosa
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+import moviepy.editor as mp
 
 app = Flask(__name__)
 
 @app.route('/prepare_data', methods=['POST'])
 def prepare_data():
-    # Carregando arquivos de treinamento
     filenames_pt = []
     filenames_en = []
     for file in os.listdir("dublado"):
@@ -14,8 +19,6 @@ def prepare_data():
     for file in os.listdir("original"):
         if file.endswith(".mp4"):
             filenames_en.append(os.path.join("original", file))
-
-    # Tokenizando os arquivos de treinamento
     samples_pt = []
     samples_en = []
     for file in filenames_pt:
@@ -24,11 +27,7 @@ def prepare_data():
     for file in filenames_en:
         audio, sr = librosa.load(file)
         samples_en.append(audio)
-
-    # Dividindo os dados em conjuntos de treinamento e teste
     X_train, X_test, y_train, y_test = train_test_split(samples_pt, samples_en, test_size=0.2, random_state=42)
-
-    # Salvando os dados preparados
     np.save("X_train.npy", X_train)
     np.save("X_test.npy", X_test)
     np.save("y_train.npy", y_train)
@@ -37,12 +36,27 @@ def prepare_data():
 
 @app.route('/train_model', methods=['POST'])
 def train_model():
-    # Carregando os dados de treinamento
     X_train = np.load("X_train.npy")
     y_train = np.load("y_train.npy")
-
-    # Construindo e treinando o modelo
     model = Sequential()
-    # Adicionando camadas aqui ...
+    model.add(LSTM(256, input_shape=(X_train.shape[1], X_train.shape[2])))
+    model.add(Dense(y_train.shape[1]))
     model.compile(optimizer='adam', loss='mse')
     model.fit(X_train, y_train, epochs=50)
+    model.save("dub_model.h5")
+    return jsonify({"message": "Modelo treinado e salvo com sucesso!"})
+
+@app.route('/dub_video', methods=['POST'])
+def dub_video():
+    
+video, sr = librosa.load("path/to/video.mp4")
+video = np.expand_dims(video, axis=0)
+dubbed_audio = model.predict(video)
+librosa.output.write_wav("dubbed_video.wav", dubbed_audio[0], sr)
+
+
+video = mp.VideoFileClip("path/to/video.mp4")
+audio = mp.AudioFileClip("dubbed_video.wav")
+video = video.set_audio(audio)
+video.write_videofile("dubbed_video.mp4")
+return jsonify({"message": "Vídeo dublado com sucesso!"})
